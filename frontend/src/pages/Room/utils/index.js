cc0369a2b17d2cb3efbb86589fae5084d9993b2a
dogukan <dgkanygn@gmail.com>
2026-01-29 00:48:42 +0300

    fix: support extracting video id from multiple youtube urls

--- frontend/src/pages/Room/utils/index.js
+++ frontend/src/pages/Room/utils/index.js
@@ -1,9 +1,16 @@
 /**
+ * YouTube video ID'sini URL'den çıkarır
+ * Desteklenen formatlar:
+ * - youtube.com/watch?v=VIDEO_ID
+ * - youtube.com/live/VIDEO_ID
+ * - youtu.be/VIDEO_ID
+ * - youtube.com/embed/VIDEO_ID
+ * - youtube.com/shorts/VIDEO_ID
  * @param {string} url 
  * @returns {string|null} 
  */
 export const extractVideoId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|live\/|shorts\/)([^#&?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 };
